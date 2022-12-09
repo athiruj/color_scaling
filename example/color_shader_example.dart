cb07c76182806b22f931a1a04c28faebc68d3a16
Athirujkaewseesuk <athirujkaewseesuk>
2022-12-10 04:04:55 +0700

    update 3

--- example/color_shader_example.dart
+++ example/color_shader_example.dart
@@ -1,6 +1,47 @@
-import 'package:color_shader/color_shader.dart';
+import '../lib/color_shader.dart';
+import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/binding.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:flutter/src/widgets/container.dart';
 
 void main() {
-  var awesome = ColorShader(0xffff0000);
-  // print('awesome: ${awesome.isAwesome}');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ColorShader',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<int> color = ColorShader(0xff0000).shaderGen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ColorShader'),
+      ),
+      body: Center(child: Container(
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: List.generate(color.length, (index) => Container(height: 50,width: 50,color: Color(color[index]))),
+        ),
+      )),
+    );
+  }
 }
--- lib/src/color_shader_base.dart
+++ lib/src/color_shader_base.dart
@@ -1,17 +1,19 @@
-import 'package:flutter/material.dart';
 
-// Color Shader tools
+/// `ColorShader`
 class ColorShader {
+  final int value;
+  static const opcity = 0xff000000;
+
   /// Normaly Color Construct is from the lower 32 bits
-  /// but [ColorShade] not support alpha value in hex or opcity
-  /// so [ColorShade] Construct is from the lower 24 bits
+  /// but [ColorShader] not support `opcity` or `alpha value` in hex 
+  /// so [ColorShader] Construct is from the lower 24 bits
   ///
   /// The bits are interpreted as follows:
   ///
-  /// * X Bits 24-31 are the alpha value.
-  /// * / Bits 16-23 are the red value.
-  /// * / Bits 8-15 are the green value.
-  /// * / Bits 0-7 are the blue value.
+  /// * ❌ Bits 24-31 are the alpha/opcity value.
+  /// * ✅ `Bits 16-23 are the red value.`
+  /// * ✅ `Bits 8-15 are the green value.`
+  /// * ✅ `Bits 0-7 are the blue value.`
   ///
   /// In other words, if 'RR' the red value in hex, 'GG' the green value in hex,
   /// and 'BB' the blue value in hex, a color can be
@@ -31,86 +33,88 @@ class ColorShader {
   /// See also [fromRGB], which takes the alpha value as a floating point
   /// value.
   ///
-  /// For example, ColorShade(255, 56, 211).
-  /// 255 is the Red value in hex, 56 is the Green value in hex,
-  /// and 211 is the Blue value in hex.
+  /// For example, `const ColorShade(255, 56, 211)`.
+  /// `255` is the Red value in hex,
+  ///  `56` is the Green value in hex,
+  /// and `211` is the Blue value in hex.
   const ColorShader.fromRGB(
     int r,
     int g,
     int b,
   ) : value = (0xff000000 | (r << 16) | (g << 8) | (b << 0)) & 0xffffffff;
 
-  final int value;
 
   /// The red channel of this color in an 8 bit value.
-  int get red => (0x00ff0000 & value) >> 16;
+  int get r => (0x00ff0000 & value) >> 16;
 
   /// The red channel of this color in an 8 bit value.
-  int get green => (0x0000ff00 & value) >> 8;
+  int get g => (0x0000ff00 & value) >> 8;
 
   /// The red channel of this color in an 8 bit value.
-  int get blue => (0x000000ff & value) >> 0;
+  int get b => (0x000000ff & value) >> 0;
 
-  List<Color> shader({int? lightLv, int? darkLv, double? scale}) {
-    return lightShader(level: lightLv ?? 4, scale: scale).reversed.toList() +
-        darkShader(level: darkLv ?? 5, scale: scale).sublist(1);
+  List<int> shaderGen({int lightLv = 4, int darkLv = 5, double scale = 1}) {
+    return lightShader(lightLv: lightLv, scale: scale).reversed.toList() +
+        darkShader(darkLv: darkLv, scale: scale).sublist(1);
   }
 
-  List<Color> lightShader({required int level, double? scale}) {
-    return List.generate(level + 1,
-        (int idx) => lightShading(level: level, pos: idx, scale: scale));
+  List<int> lightShader({required int lightLv, double scale = 1}) {
+    return List.generate(lightLv + 1,
+        (int idx) => lightShading(lightLv: lightLv, pos: idx, scale: scale));
   }
 
-  List<Color> darkShader({required int level, double? scale}) {
-    return List.generate(level + 1,
-        (int idx) => darkShading(level: level, pos: idx, scale: scale));
+  List<int> darkShader({required int darkLv, double scale = 1}) {
+    return List.generate(darkLv + 1,
+        (int idx) => darkShading(darkLv: darkLv, pos: idx, scale: scale));
   }
 
-  /// Return new Color with lighter shade
-  Color darkShading(
-      {
+/// Return new Color with lighter shade
+int lightShading({
 
-      /// [level] is 'dark level scales'
-      required int level,
+      /// [lightLv] is `Light Level`
+      required int lightLv,
 
-      /// [pos] is position of Color in 'dark level scales'
+      /// [pos] is position of Color in `Light level`
       required int pos,
 
       /// [scale]
-      double? scale}) {
-    assert(level != 0, 'dark shade level was zero.');
-    assert(level >= pos && pos < 0, 'position was over dark shade level');
-    assert(scale! >= 0.0, 'scale was less 0.0');
-    assert(scale! <= 1.0, 'scale was over 1.0');
-    scale = scale ?? 1.0;
-    return Color(0xff000000 |
-        ((red - (red ~/ (level + 1) * scale * pos)).toInt() << 16) |
-        ((green - (green ~/ (level + 1) * scale * pos)).toInt() << 8) |
-        ((blue - (blue ~/ (level + 1) * scale * pos)).toInt() << 0) &
-            0xffffffff);
+      double scale = 1}) {
+
+    assert(lightLv != 0, 'light shade level was zero.');
+    assert(lightLv >= pos && pos < 0, 'position was over light shade level');
+    assert(scale >= 0.0, 'scale was less 0.0');
+    assert(scale <= 1.0, 'scale was over 1.0');    
+    return (0xff000000                                              |
+           ((r + (r ~/ (lightLv + 1) * scale * pos)).toInt() << 16) |
+           ((g + (g ~/ (lightLv + 1) * scale * pos)).toInt() << 08) |
+           ((b + (b ~/ (lightLv + 1) * scale * pos)).toInt() << 00) & 0xffffffff);
   }
 
   /// Return new Color with lighter shade
-  Color lightShading(
-      {
+  int darkShading({
 
-      /// [level] is 'light level scales'
-      required int level,
+      /// [darkLv] is `Dark Level`
+      required int darkLv,
 
-      /// [pos] is position of Color in 'light level scales'
+      /// [pos] is position of Color in `Dark Level`
       required int pos,
 
       /// [scale]
-      double? scale}) {
-    assert(level != 0, 'light shade level was zero.');
-    assert(level >= pos && pos < 0, 'position was over light shade level');
-    assert(scale! >= 0.0, 'scale was less 0.0');
-    assert(scale! <= 1.0, 'scale was over 1.0');
-    scale = scale ?? 1.0;
-    return Color(0xff000000 |
-        ((red + ((0xff - red) ~/ (level + 1) * scale * pos)).toInt() << 16) |
-        ((green + ((0xff - green) ~/ (level + 1) * scale * pos)).toInt() << 8) |
-        ((blue + ((0xff - blue) ~/ (level + 1) * scale * pos)).toInt() << 0) &
-            0xffffffff);
+      double scale = 1}) {
+
+    assert(darkLv != 0, 'dark shade level was zero.');
+    assert(darkLv >= pos && pos < 0, 'position was over dark shade level');
+    assert(scale >= 0.0, 'scale was less 0.0');
+    assert(scale <= 1.0, 'scale was over 1.0');
+    return (0xff000000                                             |
+           ((r - (r ~/ (darkLv + 1) * scale * pos)).toInt() << 16) |
+           ((g - (g ~/ (darkLv + 1) * scale * pos)).toInt() << 08) |
+           ((b - (b ~/ (darkLv + 1) * scale * pos)).toInt() << 00) & 0xffffffff);
   }
+
+//  @override
+//   int get hashCode => value.hashCode;
+
+  // @override
+  // String toString() => '0x${value.toRadixString(16).padLeft(8, '0')}';
 }
