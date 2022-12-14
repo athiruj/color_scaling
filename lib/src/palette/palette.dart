3ba0a2a854e9bf948aefade2e4bc5f85310150f2
Athirujkaewseesuk <athirujkaewseesuk>
2022-12-14 22:08:10 +0700

    Palette Class

---
+++ lib/src/palette/palette.dart
@@ -0,0 +1,45 @@
+part of color_shader;
+
+class Palette extends ColorSwatch<int>{
+  /// Creates a color swatch with a variety of shades.
+  ///
+  /// The `primary` argument should be the 32 bit ARGB value of one of the
+  const Palette(super.primary, super.swatch);
+
+  /// The White.
+  Color get shade0 => this[0]!;
+
+  /// The lightest shade.
+  Color get shade50 => this[50]!;
+
+  /// The second lightest shade.
+  Color get shade100 => this[100]!;
+
+  /// The third lightest shade.
+  Color get shade200 => this[200]!;
+
+  /// The fourth lightest shade.
+  Color get shade300 => this[300]!;
+
+  /// The fifth lightest shade.
+  Color get shade400 => this[400]!;
+
+  /// The default shade.
+  Color get shade500 => this[500]!;
+
+  /// The fourth darkest shade.
+  Color get shade600 => this[600]!;
+
+  /// The third darkest shade.
+  Color get shade700 => this[700]!;
+
+  /// The second darkest shade.
+  Color get shade800 => this[800]!;
+
+  /// The darkest shade.
+  Color get shade900 => this[900]!;
+
+  /// The Black.
+  Color get shade1000 => this[1000]!;
+
+}
