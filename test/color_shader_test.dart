6066a64eeeb4a809182dd2d159364678f47f8840
Athirujkaewseesuk <athirujkaewseesuk>
2022-12-14 22:51:43 +0700

    update

--- test/color_shader_test.dart
+++ test/color_shader_test.dart
@@ -3,15 +3,42 @@ import 'package:color_shader/color_shader.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Color(0xffff0000);
-
-    setUp(() {
-      // Additional setup goes here.
+  group('Palette Propertie Test', () {
+    var palette = Shader(0xffff0000).palette();
+    test('palette', () {
+      print(palette.value);
+      expect(palette.shade500.value, equals(4294901760));
+    });
+    test('palette[]', () {
+      print(palette[500]!.value);
+      expect(palette[500]!.value, equals(4294901760));
     });
 
-    test('toInt test', () {
-      expect(awesome.toInt(), equals(4294901760));
+
+  });
+
+  group('Palette List Propertie Test', () {
+    List<Color> palette = Shader(0xffff0000).paletteList();
+    test('palette lenght', () {
+      print(palette.length);
+      expect(palette.length, equals(1));
     });
+
+    test('palette value', () {
+      print(palette[0].value);
+      expect(palette[0].value, equals(4294901760));
+    });
+
+    test('palette to List<int>', () {
+      print(palette.toListInt());
+      expect(palette.toListInt(), equals([4294901760]));
+    });
+    
+    test('palette to List<String>', () {
+      print(palette.toListString());
+      expect(palette.toListString(), equals(["0xffff0000"]));
+    });
+
+
   });
 }
