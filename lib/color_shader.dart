b7183dd8d2025b1a43220f37400fcd26a943298c
Athirujkaewseesuk <athirujkaewseesuk>
2022-12-14 22:49:41 +0700

    Color Shader Libeary

--- lib/color_shader.dart
+++ lib/color_shader.dart
@@ -1,5 +1,11 @@
 library color_shader;
 
+// import 'dart:collection';
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 
 part 'src/color_shader_base.dart';
+part 'src/palette/palette.dart';
+part 'src/palette/palette_list.dart';
+part 'src/palette/palette_map.dart';
