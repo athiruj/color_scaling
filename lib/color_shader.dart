24713a5c3ef65073d2951a59d833e79c28ab6433
athirujkaewseesuk <athirujkaewseesuk@gmail.com>
2022-12-17 21:35:21 +0700

    Color Shader (Complete)

--- lib/color_shader.dart
+++ lib/color_shader.dart
@@ -6,6 +6,4 @@ import 'dart:core';
 import 'package:flutter/material.dart';
 
 part 'src/color_shader_base.dart';
-part 'src/palette/palette.dart';
-part 'src/palette/palette_list.dart';
-part 'src/palette/palette_map.dart';
+part 'src/extension/palette_list.dart';
